6837e7b2ea3f067e5677ce8268d235b3cfa79840
Laurel Rubel <laurel.rubel@gmail.com>
2023-05-02 10:11:56 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const element = document.querySelector('main#main');
+element.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "LAUREL-RUBEL is the champion";
